4d7613a05591019cc14128064d6ff678eefd6b70
此处填写你注册时的用户名 <>
2020-08-07 09:27:06 +0800

    Site updated: 2020-08-07 09:27:03

--- js/beautiful.js
+++ js/beautiful.js
@@ -1,3 +1,9 @@
+// 去除banner
+var full_page = document.getElementsByClassName("full_page");
+if (full_page.length != 0) {
+  full_page[0].style.background = "transparent";
+}
+
 /*手机侧边栏默认不展开*/
 var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
 var menus_item_child = mobile_sidebar_menus.getElementsByClassName(
